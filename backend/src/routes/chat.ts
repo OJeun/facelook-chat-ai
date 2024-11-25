e94a105e3d5ade4a3ba62e15232575b9dc3563cb
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-11-25 10:31:44 -0800

    Retrieve chat from redis and then retrieve the rest from mysql

--- backend/src/routes/chat.ts
+++ backend/src/routes/chat.ts
@@ -1,28 +1,53 @@
 import { FastifyInstance } from "fastify";
-import { saveMessage, getMessagesFromDB } from "../services/databaseService";
+import {
+  saveMessagesToDB,
+  getMessagesFromDB,
+} from "../services/databaseService";
+import { getRecentMessages } from "../redis/message";
 
 export function registerChatRoutes(server: FastifyInstance) {
-    server.post("/api/chat/saveChats", async (request, response) => {
-        const { groupId, chatList } = request.body as any;
+  server.post("/api/chat/saveChats", async (request, response) => {
+    const { groupId, chatList } = request.body as any;
 
-        try {
-            await saveMessage(groupId, chatList);
-            response.send({ success: true });
-        } catch (error) {
-            response.status(500).send({ success: false, message: "Failed to save chats" });
-        }
-    });
+    try {
+      await saveMessagesToDB(groupId, chatList);
+      response.send({ success: true });
+    } catch (error) {
+      response
+        .status(500)
+        .send({ success: false, message: "Failed to save chats" });
+    }
+  });
 
-    server.get("/api/chat/getChats", async (request, reply) => {
-        const { groupId, offset, limit } = request.query as { groupId: string, offset: string, limit: string };
-        const parsedOffset = parseInt(offset, 10) || 0;
-        const parsedLimit = parseInt(limit, 10) || 20;
+  server.get("/api/chat/getChats", async (request, reply) => {
+    const { groupId, offset, limit } = request.query as {
+      groupId: string;
+      offset: string;
+      limit: string;
+    };
+    const parsedOffset = parseInt(offset, 10) || 0;
+    const parsedLimit = parseInt(limit, 10) || 20;
 
-        try {
-            const messages = await getMessagesFromDB(groupId, parsedOffset, parsedLimit);
-            reply.send({ success: true, messages });
-        } catch (error) {
-            reply.status(500).send({ success: false, message: "Failed to retrieve chats" });
-        }
-    });
+    const recentMessages = await getRecentMessages(groupId, parsedLimit);
+    const recentCount = recentMessages.length;
+
+    if (recentCount >= parsedLimit) {
+      return reply.send({ messages: recentMessages });
+    }
+
+    const remaining = parsedLimit - recentCount;
+
+    try {
+      const olderMessages = await getMessagesFromDB(
+        groupId,
+        parsedOffset,
+        remaining
+      );
+      reply.send({ messages: [...olderMessages, ...recentMessages] });
+    } catch (error) {
+      reply
+        .status(500)
+        .send({ success: false, message: "Failed to retrieve chats" });
+    }
+  });
 }
