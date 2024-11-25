97269cba77bae45fa6c093578d9a036650fae7e1
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-11-25 10:34:34 -0800

    Add websocket function for retrieve and broadcast chat

---
+++ backend/src/sockets/websocket.ts
@@ -0,0 +1,58 @@
+import { FastifyInstance } from "fastify";
+import { redisClient } from "../redis/client";
+import { saveMessage, getRecentMessages, clearMessages } from "../redis/message";
+import { WebSocket } from "ws";
+import { dumpMessagesToDB } from "../redis/dumpService";
+
+const connectedClients: Record<string, Set<WebSocket>> = {};
+const dumpTimers: Record<string, NodeJS.Timeout> = {};
+
+export function setupWebsocket(server: FastifyInstance) {
+    server.get("/ws", { websocket: true }, async (connection, req) => {
+        const { groupId } = req.query as { groupId: string };
+        
+        if (!groupId) {
+            connection.close();
+            return;
+        }
+
+        if (!connectedClients[groupId]) {
+            connectedClients[groupId] = new Set();
+
+            dumpTimers[groupId] = setInterval(() => {
+                dumpMessagesToDB(groupId);
+            }, 10 * 60 * 1000);
+        }
+
+        connectedClients[groupId].add(connection);
+
+        // Server -> Client, send recent messges to one who just connected
+        const recentMessages = await getRecentMessages(groupId);
+        connection.send(JSON.stringify({ type: "recentMessages", messages: recentMessages }));
+
+        // Client -> Server
+        connection.on("message", async (messageBuffer) => {
+            const message = JSON.parse(messageBuffer.toString());
+            await saveMessage(message);
+
+            // broadcast message to all connected clients
+            for (const client of connectedClients[groupId]) {
+                if (client.readyState == WebSocket.OPEN) {
+                    client.send(JSON.stringify({ type: "newMessage", message }));
+                }
+            }
+        }
+        )
+
+        connection.on("close", async () => {
+            connectedClients[groupId].delete(connection);
+
+            if (connectedClients[groupId].size === 0) {
+                clearInterval(dumpTimers[groupId]);
+                delete dumpTimers[groupId];
+                await dumpMessagesToDB(groupId);
+                await clearMessages(groupId);
+            }
+        });
+    });
+}
