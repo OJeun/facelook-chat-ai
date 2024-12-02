7d5662dfbff0e949ac33885197bb6ad104700b0c
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-12-02 12:47:09 -0800

    Specify the return type of getRecentMessage funciton

--- backend/src/redis/message.ts
+++ backend/src/redis/message.ts
@@ -15,7 +15,7 @@ export async function saveMessage(message: Message) {
 }
 
 // Retrieve the most recent 20 messages from a group
-export async function getRecentMessages(groupId: string, limit: number = 20): Promise<any[]> {
+export async function getRecentMessages(groupId: string, limit: number = 20): Promise<MessageWithTimestamp[]> {
   const messages = await redisClient.lRange(`group:${groupId}:messages`, -limit, -1);
   return messages.map((msg) => JSON.parse(msg));
 }
