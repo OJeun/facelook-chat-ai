000c006d01ca5bad81e6268a06ea85274007f7f5
Julie Oh <ohjeoung5224@gmail.com>
2024-12-04 11:11:38 -0800

    Make redis allow all ip address under development env

--- backend/src/redis/client.ts
+++ backend/src/redis/client.ts
@@ -4,6 +4,7 @@ import { createClient } from "redis";
 export const redisClient = createClient({ url: process.env.REDIS_URL });
 
 export async function initializeRedis() {
+  console.log(process.env.REDIS_URL);
   try {
     await redisClient.connect();
     console.log("Redis connected successfully");
