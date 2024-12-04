eb9ca5aeb35832096385581c0f96aa82ac95430e
Julie Oh <ohjeoung5224@gmail.com>
2024-12-04 11:20:52 -0800

    Fix improper way to use config environment

--- backend/src/redis/client.ts
+++ backend/src/redis/client.ts
@@ -1,5 +1,8 @@
 // src/redis/client.ts
 import { createClient } from "redis";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const redisClient = createClient({ url: process.env.REDIS_URL });
 
