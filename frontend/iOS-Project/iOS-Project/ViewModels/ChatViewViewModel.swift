0932192ff7ba9795116cd4d879fbed38d4c00888
Julie Oh <ohjeoung5224@gmail.com>
2024-12-04 08:57:42 -0800

    Make websocket request not include token

--- frontend/iOS-Project/iOS-Project/ViewModels/ChatViewViewModel.swift
+++ frontend/iOS-Project/iOS-Project/ViewModels/ChatViewViewModel.swift
@@ -23,21 +23,14 @@ class ChatViewViewModel: ObservableObject {
         webSocketTask?.cancel(with: .goingAway, reason: nil)
     }
 
+
     func connectWebSocket() {
+
         guard let url = URL(string: "wss://ios-project.onrender.com/ws?groupId=\(String(groupId))") else {
             print("Invalid WebSocket URL")
             return
         }
-        
-        guard let token = UserDefaults.standard.string(forKey: "authToken") else {
-            print("Unauthorized token")
-            return
-        }
-        
-        var request = URLRequest(url: url)
-        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-
-        webSocketTask = URLSession.shared.webSocketTask(with: request)
+        webSocketTask = URLSession.shared.webSocketTask(with: url)
         webSocketTask?.resume()
 
         receiveMessage()
