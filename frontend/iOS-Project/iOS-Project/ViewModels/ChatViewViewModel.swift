6da052f94b9c8b8abe174bb1288cb24598a3e234
GLOBALYZ <GlobalYZ@139.com>
2024-12-06 04:28:28 -0800

    fix merge bug

--- frontend/iOS-Project/iOS-Project/ViewModels/ChatViewViewModel.swift
+++ frontend/iOS-Project/iOS-Project/ViewModels/ChatViewViewModel.swift
@@ -70,6 +70,7 @@ class ChatViewViewModel: ObservableObject {
                 } else if response.type == "newMessage", let messages = response.messages, let newMessage = messages.first {
                     DispatchQueue.main.async {
                         if !self.messages.contains(where: { $0.id == newMessage.id }) {
+                            self.messages.append(newMessage)
                         }
                     }
                 }
