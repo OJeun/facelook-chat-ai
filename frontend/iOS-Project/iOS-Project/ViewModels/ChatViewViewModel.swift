419a4908e87e5eed170d77863b8510041d494079
Diane Choi <ekska21@gmail.com>
2024-12-06 09:48:19 -0800

    Revert "fix merge bug"
    This reverts commit 6da052f94b9c8b8abe174bb1288cb24598a3e234.

--- frontend/iOS-Project/iOS-Project/ViewModels/ChatViewViewModel.swift
+++ frontend/iOS-Project/iOS-Project/ViewModels/ChatViewViewModel.swift
@@ -70,7 +70,6 @@ class ChatViewViewModel: ObservableObject {
                 } else if response.type == "newMessage", let messages = response.messages, let newMessage = messages.first {
                     DispatchQueue.main.async {
                         if !self.messages.contains(where: { $0.id == newMessage.id }) {
-                            self.messages.append(newMessage)
                         }
                     }
                 }
